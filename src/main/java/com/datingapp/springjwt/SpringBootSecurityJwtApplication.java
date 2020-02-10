e7f87d1a487697d19d3621ce3cee745a0a0b3c15
venky <pvenkateshnaidu@gmail.com>
2020-02-10 15:59:17 +0530

    Package Name Changed

--- src/main/java/com/datingapp/springjwt/SpringBootSecurityJwtApplication.java
+++ src/main/java/com/datingapp/springjwt/SpringBootSecurityJwtApplication.java
@@ -1,4 +1,4 @@
-package com.bezkoder.springjwt;
+package com.datingapp.springjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
